950c6efa07a1b4f71c6559a68ba88b1f4c19c0d2
jessica kanmegne <jessicatcheyanou@gmail.com>
2018-04-13 14:28:17 +0200

    second commit-injected firebase config settings in app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firease:{
+    apiKey: "AIzaSyCS3UDJKrCTqG4P6WdulyOlsMdIM5Ca_no",
+    authDomain: "lostbutfound-9f91e.firebaseapp.com",
+    databaseURL: "https://lostbutfound-9f91e.firebaseio.com",
+    projectId: "lostbutfound-9f91e",
+    storageBucket: "lostbutfound-9f91e.appspot.com",
+    messagingSenderId: "196879400028"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firease:{
+    apiKey: "AIzaSyCS3UDJKrCTqG4P6WdulyOlsMdIM5Ca_no",
+    authDomain: "lostbutfound-9f91e.firebaseapp.com",
+    databaseURL: "https://lostbutfound-9f91e.firebaseio.com",
+    projectId: "lostbutfound-9f91e",
+    storageBucket: "lostbutfound-9f91e.appspot.com",
+    messagingSenderId: "196879400028"
+  }
+
 };
